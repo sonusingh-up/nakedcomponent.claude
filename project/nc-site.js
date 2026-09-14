2cfd7ca1cca7389a80de3768bec803eb2798bf2c
Pankaj kumar singh <sonusingh.up@yahoo.com>
2026-09-14 21:10:39 +0530

    Update nc-site.js

--- project/nc-site.js
+++ project/nc-site.js
@@ -580,7 +580,6 @@
                 <li><a href="${PAGES.scoringRubric}">Scoring rubric</a></li>
                 <li><a href="${PAGES.conflictsPolicy}">Conflicts policy</a></li>
                 <li><a href="${PAGES.contact}">Contact</a></li>
-                <li><a href="${PAGES.privacyPolicy}">Privacy policy</a></li>
               </ul>
             </div>
           </div>
